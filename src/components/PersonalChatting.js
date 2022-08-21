41e7c2dfaf27a2b7b65e2af8110b624b36336de1
chaeyeonan <dkscodus1204@ewhain.net>
2022-08-22 01:01:35 +0900

    feat: Chatting List

--- src/components/PersonalChatting.js
+++ src/components/PersonalChatting.js
@@ -3,22 +3,22 @@ import { SafeAreaView,Text,Image,View } from "react-native";
 
 const time=new Date();
 
-const PersonalChatting=()=>{
+const PersonalChatting=({title,time,message})=>{
     return(
         <SafeAreaView style={{flexDirection:'row', paddingLeft:15,paddingRight:15,paddingVertical:7}}>
 
             <Image source={require("../assets/images/carrotEx1.jpeg")}
-             style={{width:60, height:60,borderRadius:12}} />
+             style={{width:60, height:60,borderRadius:12,borderColor:'gray',borderWidth:1}} />
             <View style={{flexDirection:'column',paddingLeft:10}}>
                 <View style={{flexDirection:'row'}}>
-                    <Text style={{fontFamily:'Noto Sans KR',fontStyle:'normal',fontWeight:'700',fontSize:14,color:'#374957',top:5}}>
-                        세척 당근 반토막 나눔해요
+                    <Text style={{position:'absolute',top:7,fontFamily:'Noto Sans KR',fontStyle:'normal',fontWeight:'700',fontSize:14,color:'#374957'}}>
+                        {title}
                     </Text>
-                    <Text style={{left:'400%',fontFamily:'Noto Sans KR',fontStyle:'normal',fontWeight:'400',fontSize:10,color:'#7D7D7D',top:8}}>
-                        오전 11:48 
+                    <Text style={{position:'absolute',left:244,top:9,fontFamily:'Noto Sans KR',fontStyle:'normal',fontWeight:'400',fontSize:10,color:'#7D7D7D'}}>
+                        {time} 
                     </Text>
                 </View>
-                <Text style={{paddingTop:14,fontFamily:'Noto Sans KR',fontStyle:'normal',fontWeight:'400',fontSize:13,color:'#7D7D7D'}}>혹시 어디 쯤이신가요?</Text>
+                <Text style={{position:'absolute',top:21,left:10,paddingTop:14,fontFamily:'Noto Sans KR',fontStyle:'normal',fontWeight:'400',fontSize:13,color:'#7D7D7D'}}>{message}</Text>
             </View>
 
         </SafeAreaView>
--- src/screens/chatting/ChattingList.js
+++ src/screens/chatting/ChattingList.js
@@ -2,13 +2,37 @@ import React from "react";
 import { Pressable, SafeAreaView, ScrollView, Text } from "react-native";
 import PersonalChatting from "../../components/PersonalChatting";
 
+const List = [
+    {
+        key:1,
+        title:'세척 당근 반토막 나눔해요',
+        time:'오전 11:40',
+        lastMessage:'혹시 어디쯤이신가요?',
+    },
+    {
+        key:2,
+        title:'양파 반쪽 나눔~~',
+        time:'오전 11:45',
+        lastMessage:'도착하셨나요?',
+    },
+    {
+        key:3,
+        title:'당근 나눔해요',
+        time:'오후 7:40',
+        lastMessage:'이따 뵐게요~',
+    }
+]
+
 const ChattingList=({navigation})=>{
     return(
         <SafeAreaView style={{paddingTop:10,height:'100%',backgroundColor:'#FFFFFF'}}>
             <ScrollView style={{backgroundColor:'#FFFFFF'}}>
-            <Pressable onPress={()=>{navigation.navigate('ChattingBubble')}}>
-                <PersonalChatting/>
-            </Pressable>
+                <Pressable onPress={()=>{navigation.navigate('ChattingBubble')}}>
+                    {List.map(list=>(
+                            <PersonalChatting title={list.title} time={list.time} message={list.lastMessage} />
+                        )
+                    )}
+                </Pressable>
             </ScrollView>
         </SafeAreaView>
     )
